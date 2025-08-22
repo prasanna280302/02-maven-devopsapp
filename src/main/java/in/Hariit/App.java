d23376972adead1b15fb600308e6660f2e598422
prasannakoppisetti9@gmail.com <prasannakoppisetti9@gmail.com>
2025-08-22 18:43:56 +0530

    my first commit

---
+++ src/main/java/in/Hariit/App.java
@@ -0,0 +1,13 @@
+package in.Hariit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
